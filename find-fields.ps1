ddae00e89feba885330fe4db759f38695ba4b3c1
Leonty Chudinov <leonty.chudinov@gmail.com>
2024-01-10 16:21:30 +0500

    Main properties panel found

--- find-fields.ps1
+++ find-fields.ps1
@@ -12,10 +12,6 @@ Add-Type @"
     }
 "@
 
-# $calc = [Diagnostics.Process]::Start('calc')
-#wait for the UI to appear
-# $null = $calc.WaitForInputIdle(5000)
-# Start-Sleep -s 2
 $iikoWindowId = ((Get-Process).where{$_.MainWindowTitle -eq 'Кондитория (Лаборатория вкуса ЕКБ (УФА), Лаборатория Вкуса ЕКБ, Лаборатория Вкуса ЕКБ (ТМН))  - iikoChain 2022'})[0].Id
 Write-Output $iikoWindowId
 
@@ -28,31 +24,31 @@ Write-Output $iikoUI
 # $hwnd = $iikoUI.Current.NativeWindowHandle
 # [User32]::SetForegroundWindow($hwnd)
 
-# $window = $iikoUI.GetCurrentPattern([Windows.Automation.WindowPattern]::Pattern)
-# $window.SetWindowVisualState([System.Windows.Automation.WindowVisualState]::Normal)
-# Start-Sleep -s 2
+$window = $iikoUI.GetCurrentPattern([Windows.Automation.WindowPattern]::Pattern)
+$window.SetWindowVisualState([System.Windows.Automation.WindowVisualState]::Normal)
+Start-Sleep -s 2
 
 
-function FindPanel() {
-	Start-Sleep -s 2
-	$condition = New-Object Windows.Automation.PropertyCondition([Windows.Automation.AutomationElement]::NameProperty, "Новая расходная накладная")
-	$panel = $iikoUI.FindFirst([Windows.Automation.TreeScope]::Descendants, $condition)
-	return $panel
-}
+# function FindPanel() {
+# 	Start-Sleep -s 2
+# 	$condition = New-Object Windows.Automation.PropertyCondition([Windows.Automation.AutomationElement]::NameProperty, "Новая расходная накладная")
+# 	$panel = $iikoUI.FindFirst([Windows.Automation.TreeScope]::Descendants, $condition)
+# 	return $panel
+# }
 
 
-$panel = FindPanel
-if ($null -eq $panel) {
-	Write-Host "panel not found"
-	exit
-}
-Write-Host "panel is" $panel.Current.Name $panel.Current.ClassName
+# $panel = FindPanel
+# if ($null -eq $panel) {
+# 	Write-Host "panel not found"
+# 	exit
+# }
+# Write-Host "panel is" $panel.Current.Name $panel.Current.ClassName
 
 function FindDateField($panel){
 	Start-Sleep -s 2
 	# $condition1 = New-Object Windows.Automation.PropertyCondition([Windows.Automation.AutomationElement]::ClassNameProperty, "WindowsForms10.Window.8.app.0.35e60a0_r6_ad1")
-	# $condition1 = New-Object Windows.Automation.PropertyCondition([Windows.Automation.AutomationElement]::AutomationIdProperty, "labelDateIncoming")
-	$condition1 = New-Object Windows.Automation.PropertyCondition([Windows.Automation.AutomationElement]::NameProperty, "Дата и время выдачи товара:")
+	# $condition1 = New-Object Windows.Automation.PropertyCondition([Windows.Automation.AutomationElement]::AutomationIdProperty, "dateEditIncomingDateTime")
+	$condition1 = New-Object Windows.Automation.PropertyCondition([Windows.Automation.AutomationElement]::NameProperty, "Основные свойства")
 	# $condition2 = New-Object Windows.Automation.PropertyCondition([Windows.Automation.AutomationElement]::AutomationIdProperty, "gridInvoice")
 	# $condition2 = New-Object Windows.Automation.PropertyCondition([Windows.Automation.AutomationElement]::AutomationIdProperty, "gridInvoice")
 	# $condition = New-Object Windows.Automation.AndCondition($condition1, $condition2)
@@ -69,40 +65,8 @@ function FindDateField($panel){
 	# $grid.GetCurrentPattern([Windows.Automation.GridPattern]::Pattern).Invoke()
 }
 
-function FindCell(){
-	Start-Sleep -s 2
-	$condition1 = New-Object Windows.Automation.PropertyCondition([Windows.Automation.AutomationElement]::NameProperty, "Панель данных")
-	# $condition2 = New-Object Windows.Automation.PropertyCondition([Windows.Automation.AutomationElement]::NameProperty, "таблицу")
-	# $condition2 = New-Object Windows.Automation.PropertyCondition([Windows.Automation.AutomationElement]::AutomationIdProperty, "gridInvoice")
-	# $condition2 = New-Object Windows.Automation.PropertyCondition([Windows.Automation.AutomationElement]::AutomationIdProperty, "gridInvoice")
-	# $condition = New-Object Windows.Automation.AndCondition($condition1, $condition2)
-	$cell = $grid.FindFirst([Windows.Automation.TreeScope]::Descendants, $condition1)
-	if ($null -ne $cell) {
-		Write-Host "cell found" $cell
-	} else {
-		Write-Host "cell not found"
-		exit
-	}
-	return $cell
-	# $button.GetCurrentPattern([Windows.Automation.InvokePattern]::Pattern).Invoke()
-	# $grid.GetCurrentPattern([Windows.Automation.GridPattern]::Pattern).Invoke()
-}
-
 $dateField = FindDateField $panel
-
 Write-Host "dateField is $($dateField.Current.Name) $($dateField.Current.ClassName)"
-exit
-$cell = FindCell
 
-
-#get and click the buttons for the calculation
-
-FindAndClickButton Пять
-FindAndClickButton Плюс
-FindAndClickButton Девять
-FindAndClickButton Равно
-
-#get the result
-$condition = New-Object Windows.Automation.PropertyCondition([Windows.Automation.AutomationElement]::AutomationIdProperty, "CalculatorResults")
-$result = $iikoUI.FindFirst([Windows.Automation.TreeScope]::Descendants, $condition)
-$result.current.name
+$dateField.SetFocus()
+Start-Sleep -s 2
